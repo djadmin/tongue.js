9b8d3b7af8ef3a74730293015aeae962b93e25e6
Dheeraj Joshi <dheerajjoshi1991@gmail.com>
2016-03-05 15:37:05 +0530

    Add translations

---
+++ translations.js
@@ -0,0 +1,22 @@
+var Translations = (function () {
+	'use strict';
+
+	var map = {}
+
+	map['en'] = {};
+
+	map['hi'] = {
+	  "alert": "सावधान",
+	  "show": "निर्देश",
+	  "true": "सही",
+	  "function": 'काम',
+	  "var": 'मानलो',
+	  "if": 'अगर',
+	  "console": 'दिलासा',
+	  "log": 'लघुगणक'
+	};
+
+	return {
+		map: map
+	};
+})();
