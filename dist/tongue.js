5fa377bbd816705f899f204c719f90310af16207
Dheeraj Joshi <dheerajjoshi1991@gmail.com>
2016-06-06 15:14:19 +0530

    Use latest version and update example

--- dist/tongue.js
+++ dist/tongue.js
@@ -10,12 +10,12 @@
 
 	// Universal Module Definition (UMD)
 	if (typeof define === 'function' && define.amd) {	// AMD
-        define('tongue', ['esprima'], factory);
-    } else if (typeof exports === 'object') {	// CommonJS, Node
-        module.exports = factory(require('esprima'));
-    } else { // Browser globals (root is window)
-        root.tongue = factory(root.esprima);
-    }
+		define('tongue', ['esprima'], factory);
+	} else if (typeof exports === 'object') {	// CommonJS, Node
+		module.exports = factory(require('esprima'));
+	} else { // Browser globals (root is window)
+		root.tongue = factory(root.esprima);
+	}
 })(this, function (esprima) {
 	'use strict';
 
@@ -27,28 +27,97 @@
 		keyDiff;
 
 	var translations = (function () {
-		'use strict';
 
-		var map = {};
+		var MAP = {};
 
-		map['en'] = {};
+		MAP.en = {};
 
-		map['hi'] = {
-		  "alert": "सावधान",
-		  "show": "निर्देश",
-		  "true": "सही",
-		  "function": 'काम',
-		  "var": 'मानलो',
-		  "if": 'अगर',
-		  "console": 'दिलासा',
-		  "log": 'लघुगणक',
-		  "new": 'नया',
-		  "for": 'चलाओ',
-		  "length": 'लंबाई'
+		MAP.hi = {
+			"break": "तोड़",
+			"case": "मामला",
+			"catch": "पकड़",
+			"class": "कक्षा",
+			"const": "लगातार",
+			"continue": "जारी",
+			"debugger": "खोदना",
+			"default": "हरबार",
+			"delete": "हटा",
+			"do": "कर",
+			"else": "वरना",
+			"export": "निर्यात",
+			"extends": "फैली",
+			"finally": "आखिरकार",
+			"for": "चलाओ",
+			"function": "काम",
+			"if": "अगर",
+			"import": "लाओ",
+			"in": "में",
+			"instanceof": "उनके",
+			"new": "नया",
+			"return": "वापसी",
+			"super": "बड़े",
+			"switch": "परख",
+			"this": "उसका",
+			"throw": "फेंक",
+			"try": "कोशिश",
+			"typeof": "प्रकार",
+			"var": "मानलो",
+			"void": "शून्य",
+			"while": "जबकि",
+			"with": "साथ",
+			"yield": "प्राप्ति",
+			"alert": "सावधान",
+			"true": "सही",
+			"false": "ग़लत",
+			"length": "लंबाई",
+			"console": "तकती",
+			"log": "छापो"
+		};
+
+		MAP.cn = {
+			"break": "你打破它",
+			"case": "案件",
+			"catch": "抓住",
+			"class": "类",
+			"const": "不变",
+			"continue": "继续吧",
+			"debugger": "挖出",
+			"default": "每次",
+			"delete": "去掉",
+			"do": "做",
+			"else": "除此以外",
+			"export": "出口",
+			"extends": "扩展",
+			"finally": "最后",
+			"for": "对于",
+			"function": "功能",
+			"if": "如果",
+			"import": "把它",
+			"in": "在",
+			"instanceof": "他的",
+			"new": "新",
+			"return": "返回",
+			"super": "大",
+			"switch": "判断他",
+			"this": "他的东西",
+			"throw": "丢它",
+			"try": "尝试",
+			"typeof": "类型",
+			"var": "承担",
+			"void": "空虚",
+			"while": "而",
+			"with": "同",
+			"yield": "产量",
+			"alert": "警报",
+			"true": "真正",
+			"false": "假",
+			"length": "长度",
+			"console": "垫",
+			"log": "印"
 		};
 
 		return {
-			map: map
+			map: MAP
 		};
 	})();
 
@@ -63,11 +132,11 @@
 			}
 		}
 		return inverted;
-	};
+	}
 
 	function replaceRange(str, start, end, substitute) {
 		return str.substring(0, start) + substitute + str.substring(end);
-	};
+	}
 
 	// Generate Reverse Mapping
 	for (var loc in transMap) {
@@ -83,10 +152,11 @@
 			curlen,
 			rlen;
 
-			if (token.type !== 'Identifier') { return };
+			// Keyword will only be present when doing reverse transformation
+			if (token.type !== 'Identifier' && token.type !== 'Keyword') { return; }
 			if (replaceStr) {
 				targetCode = replaceRange(targetCode, trange.s,  trange.e, replaceStr);
-				curlen = range[1] - range[0], rlen = replaceStr.length;
+				curlen = range[1] - range[0]; rlen = replaceStr.length;
 				keyDiff = keyDiff + (rlen - curlen);
 			}
 	}
@@ -105,7 +175,11 @@
 		options = options || {};
 		locale = options.locale || 'en';
 		map = options.map ? invert(options.map) : inverseMap[locale];
-
+		// Support reverse transformation from English to other languages
+		// TODO: Add support for transforming from A to B where A, B is other than English
+		if (options.targetLocale) {
+			map = transMap[options.targetLocale];
+		}
 		// start scanning
 		try {
 			scan();
@@ -114,10 +188,10 @@
 		}
 
 		return targetCode;
-	};
+	}
 
 	return {
-		version: '0.1.0',
+		version: '0.1.1',
 		transform: transform,
 		map: transMap
 	};
